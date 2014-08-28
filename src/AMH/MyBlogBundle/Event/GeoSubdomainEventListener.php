39ceb953a7c3b9b88a2b01266c50648784145ae7
AlexMaxHorkun <mindkilleralexs@gmail.com>
2014-08-28 20:50:20 +0300

    geo detection and redirection to subdomain

--- src/AMH/MyBlogBundle/Event/GeoSubdomainEventListener.php
+++ src/AMH/MyBlogBundle/Event/GeoSubdomainEventListener.php
@@ -1,13 +1,32 @@
 <?php
 namespace AMH\MyBlogBundle\Event;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use AMH\MyBlogBundle\Util\GeoDetector;
+use Symfony\Component\Routing\Router;
+
 /**
  * Class GeoSubdomainEventListener
  * @package AMH\MyBlogBundle\Event
  * @author Alexander Horkun mindkilleralexs@gmail.com
  */
 class GeoSubdomainEventListener {
+    /**
+     * @var GeoDetector
+     */
+    private $geoDetector;
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function __construct(GeoDetector $detector, Router $router){
+        $this->geoDetector=$detector;
+        $this->router=$router;
+    }
+
     /**
      * @param GetResponseEvent $event
      * @return void
@@ -17,8 +36,14 @@ class GeoSubdomainEventListener {
             return;
         }
         /** @var string $locale */
-        $locale=$event->getRequest()->getLocale();
-        $host=$event->getRequest()->getHost();
-        print_r($locale." ".$host); die();
+        $geoParam=$event->getRequest()->get('_geo');
+        $geo=$this->geoDetector->byIp($event->getRequest()->getClientIp());
+        if($geo && $geo!=$geoParam){
+            $routeParams=$this->router->match($event->getRequest()->getPathInfo());
+            $routeParams['_geo']=$geo;
+            $routeName=$routeParams['_route'];
+            unset($routeParams['_route']);
+            $event->setResponse(new RedirectResponse($this->router->generate($routeName, $routeParams)));
+        }
     }
 } 
